138f1f0a92523685cfc483ff0b1481cc6eba5857
Kevin Funderburg <ksfunderburg@icloud.com>
2019-09-17 02:30:59 -0500

    added command line parsing, started event processessing

--- sim.cpp
+++ sim.cpp
@@ -2,21 +2,25 @@
 #include <cstdlib>
 #include <cmath>
 //#include <ctime>
-
 using namespace std;
-// TODO - add states of the CPU to the system
+
+#define SIZE 10000
 ////////////////////////////////////////////////////////////////
-// sample events
-/// TODO - need to define what kind of events there are
-#define EVENT1 1
-#define EVENT2 2
+// TODO - need to clarify these events
+#define ARRIVE 1
+#define COMPLETE 2
+#define SLICE 3
+
 // .. add more events
+// NOTE - not sure if this is the best way
+enum State {NEW = 1, READY = 2, WAITING = 3, RUNNING = 4, TERMINATED = 5};
 
 ////////////////////////////////////////////////////////////////     //event structure
 struct event{
     float time;
     int   type;
-    // add more fields
+    int   priority;
+    int   burst;
     struct event* next;
 };
 
@@ -26,22 +30,23 @@ void init();
 int run_sim();
 void generate_report();
 int schedule_event(struct event*);
-int process_event1(struct event* eve);
+int delete_event(struct event* eve);
 int process_event2(struct event* eve);
 
 ////////////////////////////////////////////////////////////////
 //Global variables
 struct event* head; // head of event queue
 float _clock; // simulation clock, added underscore to make unique from system clock
-int end_condition;
+int process_count;
 
 ////////////////////////////////////////////////////////////////
 void init()
 {
     // initialize all variables, states, and end conditions
-//    head->time = 0.0;   // set queue time and clock to 0
-    end_condition = 10000;      // 10000 processes
-
+    head = new event;
+    head->time = 0.0;
+    event processes[SIZE];
+    process_count = 0;    // end condition, ends when it equals 10000
     // schedule first events
     schedule_event(head);
 }
@@ -51,9 +56,22 @@ void generate_report()
     // output statistics
 }
 //////////////////////////////////////////////////////////////// //schedules an event in the future
-int schedule_event(struct event*)
+int schedule_event(event *newEvent)
 {
-// insert event in the event queue in its order of time
+    // insert event in the event queue in its order of time
+    event* temp;
+    temp = head;
+
+    // this appends to the list, not sure if that's what's needed
+    newEvent->next = nullptr;
+    while(temp->next != nullptr)
+        temp = temp->next;
+    temp->next = newEvent;
+    return 0;
+}
+
+int delete_event(event *eve)
+{
     return 0;
 }
 ////////////////////////////////////////////////////////////////
@@ -78,55 +96,70 @@ float genexp(float lambda)
 ////////////////////////////////////////////////////////////
 int run_sim()
 {
-    struct event* eve;
-    while (!end_condition)
+    event *eve;
+//    while (!end_condition)
+    while(process_count < 10000)
     {
-        cout << "looping\n";
         eve = head;
+        cout << "eve->time: " << eve->time << endl;
         _clock = eve->time;
         switch (eve->type)
         {
-            case EVENT1:
-                process_event1(eve);
+            case ARRIVE:
+                schedule_event(eve);
                 break;
-            case EVENT2:
-                process_event2(eve);
+            case COMPLETE:
+                delete_event(eve);
                 break;
 
                 // add more events
 
             default:
-                cerr << "invalid event type";   // error
+                cerr << "invalid event type\n";   // error
         }
 
         head = eve->next;
         delete eve;
-        eve = NULL;
+        eve = nullptr;
+        process_count++;
     }
     return 0;
 }
 
-int process_event1(struct event* eve)
-{
-    // need to have arrival time
-    return 0;   // PLACEHOLDER
-}
 
 int process_event2(struct event* eve)
 {
     return 0;   // PLACEHOLDER
 }
+
+static void show_usage()
+{
+    cerr << "Usage: sim [123] [average arrival rate] [average service time] [quantum interval]\n\n"
+         << "Options:\n"
+         << "\t1 : First-Come First-Served (FCFS)\n"
+         << "\t2 : Shortest Remaining Time First (SRTF)\n"
+         << "\t3 : Round Robin, with different quantum values (RR) (requires 4 arguments)\n";
+}
 ////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[] )
 {
     clog << "starting\n";
-    if (argc == 1)
-    {
-        cerr << "sim [123] [average arrival rate] [average service time] [quantum interval]\n\n"
-             << "1 : FCFS\n"
-             << "2 : SRTF\n"
-             << "3 : RR";
+    if (argc < 3) {
+        show_usage();
+        return 1;
     }
+    // RR requires the quantum arg
+    if (*argv[2] == 3 && argc < 5) {
+        cerr << "Expected 4 arguments and got " << argc << endl;
+        show_usage();
+        return 1;
+    }
+    int scheduler = stoi(argv[1]);
+    int lambda = stoi(argv[2]);
+    float avgServiceTime = stof(argv[3]);
+    if (argc == 5)
+        float quantum = stof(argv[4]);
+
     init();
     run_sim();
     generate_report();
